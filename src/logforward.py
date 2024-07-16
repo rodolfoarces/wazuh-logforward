54cf2c1b5366e5c54b151a1256fa21bae5aca480
Rodolfo Arce Sannemann <rodolfo.arce@wazuh.com>
2024-07-16 12:00:33 -0400

    First steps using command parameters

--- src/logforward.py
+++ src/logforward.py
@@ -9,16 +9,22 @@
 # Requirements
 import sys
 import subprocess
+import argparse
 from pathlib import Path
 
 # Variables
 file_list = list()
 # Log directory
-log_dir = Path(sys.argv[1]).resolve()
+#log_dir = Path(sys.argv[1]).resolve()
 #Script directory
 script_dir = Path(__file__).resolve().parent
 
 # Functions and additional processing
+def printHelp():
+    msg = "Usage:\n logforwarder [OPTIONS] DIR"
+    print(msg)
+    quit()
+
 def findFiles(directory=sys.argv[-1]):
     files_in_dir = subprocess.run(['find', directory , '-type', 'f'], stdout=subprocess.PIPE)
     files = files_in_dir.stdout.decode('utf-8').splitlines()
@@ -27,6 +33,33 @@ def findFiles(directory=sys.argv[-1]):
 
     # print(file_list)
 
+# Read parameters using argparse
+
+# Initialize parser
+parser = argparse.ArgumentParser()
+
+# Adding optional argument
+parser.add_argument("-d", "--directory", help = "Log directory", action="store")
+parser.add_argument("-o", "--output", help = "Log output to file")
+
+# Read arguments from command line
+args = parser.parse_args()
+
+#Set the output log
+try:
+    if args.output:
+        print("Logging to: % s" % args.output)
+except AttributeError:
+    print("Logging to stout")
+
+#Set the output log
+try:
+    if args.directory:
+        print("Processing: % s" % args.directory)
+except AttributeError:
+    print("Directory option is required, use -d | --directory")
+    quit()
+
 # Main program
-findFiles(sys.argv[-1])
+#findFiles(sys.argv[-1])
 quit()
