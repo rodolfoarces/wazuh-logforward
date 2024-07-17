cc5ab1558ed42c509f06717358e62715c60bf4fc
Rodolfo Arce Sannemann <rodolfo.arce@wazuh.com>
2024-07-17 09:43:49 -0400

    Added basic authentication control on the managers API request

--- src/logforward.py
+++ src/logforward.py
@@ -5,6 +5,9 @@
 # https://oxylabs.io/blog/python-script-service-guide
 # List files and directories, TO-DO, currently using find command
 # https://realpython.com/get-all-files-in-directory-python/
+# Exit errors:
+# 1 - Required parameter is missing
+# 2 - Authentication error (Token)
 
 # Requirements
 import sys
@@ -12,8 +15,15 @@ import subprocess
 import argparse
 import requests
 from pathlib import Path
+# Disabling warning: /usr/lib/python3/dist-packages/urllib3/connectionpool.py:1100: InsecureRequestWarning: 
+# Unverified HTTPS request is being made to host '10.1.1.3'. Adding certificate verification is strongly advised.
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
 # Variables
+token = None
+username = "admin"
+password = "admin"
+manager = "https://localhost:55000"
 file_list = list()
 #Script directory
 script_dir = Path(__file__).resolve().parent
@@ -42,14 +52,19 @@ def processFile(file):
 
 def processDirectory(directory):
     findFiles(directory)
+    apiAuthenticate(manager,username,password)
     for file in file_list:
         processFile(file)
 ## API tasks
-def apiAuthenticate(url,username,password): 
+def apiAuthenticate(auth_manager,auth_username, auth_password):
+    auth_endpoint = auth_manager + "/security/user/authenticate?raw=true"
     print("Starting authentication process")
     # api-endpoint
-    #r = requests.get(url, auth=(username, password))
-    #print(str(r.content))
+    auth_request = requests.get(auth_endpoint, auth=(auth_username, auth_password), verify=False)
+    token = auth_request.content.decode("utf-8")
+    if "Unauthorized" in token:
+        print("Authentication error")
+        exit(2)
 
 # Read parameters using argparse
 ## Initialize parser
@@ -58,7 +73,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--directory", help = "Log directory (Required)", action="store")
 parser.add_argument("-u", "--username", help = "Username (Required)", action="store", default="admin")
 parser.add_argument("-p", "--password", help = "Password", action="store", default="admin")
-parser.add_argument("-U", "--url", help = "Wazuh Manager Url (Required)", action="store", default="https://localhost:55000")
+parser.add_argument("-m", "--manager", help = "Wazuh Manager Url (Required)", action="store", default="https://localhost:55000")
 parser.add_argument("-o", "--output", help = "Log output to file")
 ## Read arguments from command line
 args = parser.parse_args()
@@ -72,6 +87,22 @@ if args.directory and args.directory != None:
 else:
     print("Directory option is required, use -d | --directory")
     exit(1)
+if args.username != "admin":
+    print("Setting username")
+    username = str(args.username)
+else:
+    print("Username not set, using: %s" % username)
+if args.password != "admin":
+    print("Setting password")
+    password = str(args.password)
+else:
+    print("Password not set, using default value")
+if args.manager != "https://localhost:55000":
+    print("Setting url")
+    manager = str(args.manager)
+else:
+    print("URl not set, using: https://localhost:55000")
+
 ## Set the output log
 if args.output:
     print("Logging to: % s" % args.output)
